b1658b83d07ebca3509e037ef514dda8c70a3765
Henry <94872110+Henryking98@users.noreply.github.com>
2024-12-29 03:41:33 -1100

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,29 @@
+import Sidebar from "@/components/Sidebar";
+import MobileNavigation from "@/components/MobileNavigation";
+import Header from "@/components/Header";
+import { getCurrentUser } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
+import { Toaster } from "@/components/ui/toaster";
+
+const Layout = async ({ children }: { children: React.ReactNode }) => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) return redirect("/sign-in");
+
+  return (
+    <main className="flex h-screen">
+      {/* <Sidebar /> */}
+      <Sidebar {...currentUser} />
+
+      <section className="flex h-full flex-1 flex-col">
+        {/* <MobileNavigation /> */}
+        <MobileNavigation {...currentUser} />
+        <Header userId={currentUser.$id} accountId={currentUser.accountId}/>
+        <div className="main-content">{children}</div>
+      </section>
+
+      <Toaster />
+    </main>
+  );
+};
+export default Layout;
