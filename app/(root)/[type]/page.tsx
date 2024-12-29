bb44dadc842d4c2d97e5561cb92dda971f16d4a3
Henry <94872110+Henryking98@users.noreply.github.com>
2024-12-29 07:07:20 -1100

    implemented files dropdown action

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Sort from "@/components/Sort";
-// import { getFiles } from "@/lib/actions/file.actions";
+import { getFiles } from "@/lib/actions/file.actions";
 import { Models } from "node-appwrite";
-// import Card from "@/components/Card";
+import Card from "@/components/Card";
 import { getFileTypesParams } from "@/lib/utils";
 
 const Page = async ({ searchParams, params }: SearchParamProps) => {
@@ -12,7 +12,7 @@ const Page = async ({ searchParams, params }: SearchParamProps) => {
 
   const types = getFileTypesParams(type) as FileType[];
 
-//   const files = await getFiles({ types, searchText, sort });
+  const files = await getFiles({ types, searchText, sort });
 
   return (
     <div className="page-container">
@@ -33,15 +33,15 @@ const Page = async ({ searchParams, params }: SearchParamProps) => {
       </section>
 
       {/* Render the files */}
-      {/* {files.total > 0 ? (
+      {files.total > 0 ? (
         <section className="file-list">
           {files.documents.map((file: Models.Document) => (
-            // <Card key={file.$id} file={file} />
+            <Card key={file.$id} file={file} />
           ))}
         </section>
       ) : (
         <p className="empty-list">No files uploaded</p>
-      )} */}
+      )}
     </div>
   );
 };
